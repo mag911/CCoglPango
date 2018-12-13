f153cb20bf01b482924321480d2307ce5c29533b
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-13 15:21:03 +1000

    Fix typo

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription 
 
-let package = Package(name: "CCoglPango"
+let package = Package(name: "CCoglPango",
 	pkgConfig: "cogl-pango cogl-1.0 cogl-gl-1.0 cogl-path-1.0 pango pangoft2 cairo gobject-2.0 gio-unix-2.0 glib-2.0",
 	providers: [
 		.brew(["cogl", "cairo", "glib", "glib-networking", "gobject-introspection"]),
